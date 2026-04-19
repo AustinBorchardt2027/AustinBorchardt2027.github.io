e42b5d45cee0218fe9ddd0bb6c79ee0498b25e17
AustinBorchardt2027 <computerbug@duck.com>
2026-04-18 22:40:00 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Sheet published as CSV
 const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRk-WuFbb7q-_ZNbCjC6AaeV5yR6cGDuVCBJp0-wQI3zRQmdSaw87uzsUwI3dFgXTvsO_qBs6ach1C/pub?output=csv';
 // ↓↓ PASTE YOUR APPS SCRIPT /exec URL HERE ↓↓
-const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbx8QRBHSDR29-80kHdKZYit1nGCCrOkQiEO9takJc-Cmiq1nBXkC-L53rRDZXAbshNS/exec';
+const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwA7tPIXs9zvIPT7haaCEXC8qiZlnDjlnY4tZJglMzxs65xlDkrdRSJnAjwysgWBoZzSQ/exec';
 
 
 // ─── ACCESS KEY GATE ──────────────────────────────────────────
