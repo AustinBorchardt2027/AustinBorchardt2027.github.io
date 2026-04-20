5915ea631875240215832c752b15094d3435cf67
TheDrive2027 <computerbug@duck.com>
2026-04-20 17:22:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Sheet published as CSV
 const SHEET_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRRk-WuFbb7q-_ZNbCjC6AaeV5yR6cGDuVCBJp0-wQI3zRQmdSaw87uzsUwI3dFgXTvsO_qBs6ach1C/pub?output=csv';
 // ↓↓ PASTE YOUR APPS SCRIPT /exec URL HERE ↓↓
-const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbz_SNckeML0N2__SIGtqzPyTirNsfByPRodmEFD8yu957p-qV8VnVw4tuClyLp3KFAvcw/exec';
+const DRIVE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_GqZhTfa2Fg5e6UhGTEeVrC6C5via2O_pUEoDtgSvLBsfvurC7JPEpqkjGCof5wqGPg/exec';
 
 
 // ─── ACCESS KEY GATE ──────────────────────────────────────────
